4001612b66d45d183dfd789946bcd0dd5ac62b45
bolaji87 <sakabolaji77@gmail.com>
2024-11-05 12:24:17 +0100

    feat: add ref

--- src/Hooks/contexts/ProductsContext.jsx
+++ src/Hooks/contexts/ProductsContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchProducts, fetchCategoryProducts } from "../../utils";
 
@@ -7,6 +7,7 @@ const productsContext = createContext();
 function ProductsProvider({ children }) {
   const [allProducts, setAllProducts] = useState([]);
   const [products, setProducts] = useState([]);
+  const categoryRef = useRef(null);
 
   function handleFilterProducts(productCategory = null) {
     if (productCategory) {
@@ -26,6 +27,7 @@ function ProductsProvider({ children }) {
         products,
         setProducts,
         onFilterProducts: handleFilterProducts,
+        categoryRef,
       }}
     >
       {children}
