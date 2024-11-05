898f757dd1322d8527dea1f689291a7a3b1f7b31
bolaji87 <sakabolaji77@gmail.com>
2024-11-05 11:14:38 +0100

    feat: add path to match url in the category

--- src/components/category-card/Category.jsx
+++ src/components/category-card/Category.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./category.css";
+import { useNavigate } from "react-router-dom";
+import { ALLOEWD_CATEGORIES } from "../../route";
+
 function Category({ image, title }) {
+  const navigate = useNavigate();
   return (
     <div className="category">
       <img src={image} alt={title} />
-      <button>
-        <a href="#">{title}</a>
+      <button
+        onClick={() =>
+          navigate(
+            `/products/${
+              title.toLowerCase() === "women's clothing"
+                ? ALLOEWD_CATEGORIES.WOMENS
+                : ALLOEWD_CATEGORIES.MENS
+            }`
+          )
+        }
+      >
+        {title}
       </button>
     </div>
   );
