34ec8af98577ac8d6b227fead7008bf7dc2912fe
bolaji87 <sakabolaji77@gmail.com>
2024-12-23 14:05:30 +0100

    feat: use AuthContext in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
+import { CartProvider } from "./Hooks/contexts/CartContext.jsx";
+import { ProductsProvider } from "./Hooks/contexts/ProductsContext.jsx";
+import { UserProvider } from "./Hooks/contexts/UserContext.jsx";
+import { AuthProvider } from "./Hooks/contexts/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <CartProvider>
+          <ProductsProvider>
+            <UserProvider>
+              <App />
+            </UserProvider>
+          </ProductsProvider>
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
