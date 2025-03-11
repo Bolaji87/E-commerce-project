4790c25e511769acee8742ecada921cd6e5b378a
Bolaji87 <sakabolaji77@gmail.com>
2025-03-11 05:30:12 +0100

    feat: create Navbar component and its css file

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,1 +1,29 @@
-rfce;
+import React from "react";
+import { FaBagShopping } from "react-icons/fa6";
+
+import styles from "./Navbar.module.css";
+
+function NavBar() {
+  return (
+    <header className={styles.navBar}>
+      <h2>Horizon Hub</h2>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>About us</li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          <li>
+            <FaBagShopping className={styles.cartLogo} />
+          </li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
